c38e46e37bf3add999d20b964cd7c563ac54df99
Brisa <ccbc@hawaii.edu>
2025-02-24 22:14:47 +0000

    Sync on minor changes

--- index.js
+++ index.js
@@ -3,15 +3,14 @@
 /*
 Name: Brisa Carter
 Assignment: Week 5 - Weekend To do List
-Description: Creates a weekend list of things to do extending Debasis To Do list
+Description: Creates a list of things to do on the weekends
 Date: 02/13/25
 */
 
-
 //jshint esversion:6
 
-/* for this activity I used AI to add jest test suits and verify if server is running
-run app using  node.index
+/* for this activity I used AI to add jest test suite and verify if server is running
+run app using  node index
 - test using npm test 
 
 What I did:
@@ -21,10 +20,9 @@ What I did:
 - Searched correct path for images folder - answer from Stacker Overflow
 - Added npm nodemon to speed up restarting the server (tip from Angela Yu)
     • to restart the server without having to stop ntype
-- Searched for html code for island emoji
+- Searched for html code for hibiscus emoji
 */
 //adds express and body-parser
-//jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -33,14 +31,15 @@ const date = require(__dirname + "/date.js");
 const app = express();
 const PORT = process.env.PORT || 3000; // Use environment variable for tests
 
-let items = ["Study", "Exercise", "Play Video Games", "Sleep" ];
+let items = ["Study", "Exercise", "Play Video Games", "Sleep"];
 let weekendItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {  // Fixed typo here
+app.get("/", function (req, res) {
+  // Fixed typo here
   let today = new Date();
   let options = { weekday: "long", day: "numeric", month: "long" };
   let day = today.toLocaleDateString("en-US", options); // Store the date in a variable
@@ -48,9 +47,8 @@ app.get("/", function (req, res) {  // Fixed typo here
   res.render("list", { listTitle: day, newListItems: items }); // Send it to the template
 });
 
-
-
-app.post("/", function (req, res) {  // Fixed typo here
+app.post("/", function (req, res) {
+  // Fixed typo here
   let item = req.body.newItem;
 
   if (req.body.list === "weekend") {
@@ -62,8 +60,12 @@ app.post("/", function (req, res) {  // Fixed typo here
   }
 });
 
-app.get("/weekend", function (req, res) {  // Fixed typo here
-  res.render("list", { listTitle: "Weekend Things To Do List", newListItems: weekendItems });
+app.get("/weekend", function (req, res) {
+  // Fixed typo here
+  res.render("list", {
+    listTitle: "Weekend Things To Do List",
+    newListItems: weekendItems,
+  });
 });
 
 // Start the server only if not in test mode
